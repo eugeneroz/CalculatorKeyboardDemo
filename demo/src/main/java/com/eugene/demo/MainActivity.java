1d12d942d9a7e65036647d9365cf9fbde9b02351
Eugene Rozenberg <eugeneroz83@gmail.com>
2016-08-18 16:39:56 +0300

    Init project structure

---
+++ demo/src/main/java/com/eugene/demo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.eugene.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.eugene.demo.R.layout.activity_main);
+    }
+}
